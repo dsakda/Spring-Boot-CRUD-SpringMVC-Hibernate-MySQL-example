872720e5a4af1db040808b462d98bf2a4626a5c0
dsakda <74044765+dsakda@users.noreply.github.com>
2020-12-10 17:58:50 +0700

    First commit

---
+++ src/main/java/com/example/SpringBootCrudHibernateExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudHibernateExampleApplication.class, args);
+	}
+
+}
