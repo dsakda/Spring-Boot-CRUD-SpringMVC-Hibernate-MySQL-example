d79b548d5b56e7b8bc75559aea33b3b5aa645974
dsakda <74044765+dsakda@users.noreply.github.com>
2020-12-13 02:08:13 +0700

    Update SpringBootCrudHibernateExampleApplication.java

--- src/main/java/com/example/SpringBootCrudHibernateExampleApplication.java
+++ src/main/java/com/example/SpringBootCrudHibernateExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCrudHibernateExampleApplication {
+public class SpringBootCRUDHibernateExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCrudHibernateExampleApplication.class, args);
+		SpringApplication.run(SpringBootCRUDHibernateExampleApplication.class, args);
 	}
 
 }
